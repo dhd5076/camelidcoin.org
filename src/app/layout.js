ba7cc96345877bf0413fbb19333c2d7eb8063630
Dylan Dunn <dylandunn17@icloud.com>
2023-09-16 21:38:30 -0400

    Improved landing page

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,49 +9,33 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="w-screen, h-screen bg-bgColor">
-      <nav className="bg-gray-800">
+      <body className="h-screen bg-bgColor">
+      <nav className="bg-black/25">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                   <span className="absolute -inset-0.5"></span>
-                  <span className="sr-only">Open main menu</span>
                 </button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="CamelidCoin"/>
+                  <img className="h-10 w-auto" src="/logoLarge.png"/>
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
+                <div className="hidden sm:ml-6 sm:block my-auto">
                   <div className="flex space-x-4">
-                    <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Demo</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+                    <a href="/" className="text-gray-300 hover:bg-black/25 hover:text-white rounded-md px-3 py-2 text-sm font-bold">Home</a>
+                    <a href="#" className="text-gray-300 hover:bg-black/25 hover:text-white rounded-md px-3 py-2 text-sm font-bold">Features</a>
+                    <a href="#" className="text-gray-300 hover:bg-black/25 hover:text-white rounded-md px-3 py-2 text-sm font-bold">FAQ</a>
+                    <a href="#" className="text-gray-300 hover:bg-black/25 hover:text-white rounded-md px-3 py-2 text-sm font-bold">Contact</a>
                   </div>
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">View notifications</span>
-                </button>
-                <div className="relative ml-3">
-                  <div>
-                    <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                      <span className="absolute -inset-1.5"></span>
-                      <span className="sr-only">Open user menu</span>
-                      <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-                    </button>
+                <div className="flex space-x-4">
+                    <a href="#" className="text-gray-300 hover:bg-black/25 hover:text-white rounded-md px-3 py-2 text-sm font-bold">Sign In</a>
+                    <a href="#" className="bg-black/25 text-gray-300 hover:bg-primary/50 hover:text-black rounded-md px-3 py-2 text-sm font-bold">Create Account</a>
                   </div>
-
-                  <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
-                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Your Profile</a>
-                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1">Settings</a>
-                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">Sign out</a>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
@@ -59,13 +43,16 @@ export default function RootLayout({ children }) {
           <div className="sm:hidden" id="mobile-menu">
             <div className="space-y-1 px-2 pb-3 pt-2">
               <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Dashboard</a>
+              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Features</a>
+              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Demo</a>
             </div>
           </div>
         </nav>
         {children}
+        <div className="bg-primary text-center w-full text-xs p-2">
+          &#169; 2023 CamelCoin | Released under the GPLv3 License
+        </div>
       </body>
       <Analytics />
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,22 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-    <script type="text/javascript" async src="//l.getsitecontrol.com/94gdpgj4.js"></script>
-    <div className="mx-auto p-4 w-screen flex flex-col items-center max-w-6xl">
-      <Image src="/logoLarge.png" width={128} height={128} className="mx-auto"></Image>
-      <h1 className="text-6xl mt-4 font-sans text-primary pb-4 mx-2"> CamelidCoin</h1>
-      <div className="sm:w-full md:w-1/2">
-        <p className="pt-4 text-primary"> CamelidCoin is a blockchain protocol designed for distributed large language model computation and training. Clients submit input for completion by a pool of compute nodes who are compensated in return. To ensure output validity, we propose a new algorithm called RASTiC which quickly verifies output authenticity with near certainty. Our protocol is built on the original Bitcoin model and has been modified to fit our use case. </p>
-        <a href="/whitepaper.pdf" className='mr-auto text-white bg-black py-2 px-6 rounded mt-4 inline-block text-center font-semibold'> Read The Whitepaper &#10132;</a>
-        <br/>
-        <a href="https://github.com/dhd5076/CamelidCoin" className='mr-auto text-white bg-black py-2 px-6 rounded mt-2 inline-block text-center font-semibold'> View The Source On Github &#10132;</a>
-
-        <br/>
-        <a href="https://discord.gg/Uv3v5pJZQr" className='mr-auto text-white bg-black py-2 px-6 rounded mt-2 inline-block text-center font-semibold'> Join The Discord &#10132;</a>
+      <script type="text/javascript" async src="//l.getsitecontrol.com/94gdpgj4.js"></script>
+      <div className="mx-auto w-full relative h-screen">
+        <div className="absolute inset-0 bg-black">
+          <div className="absolute inset-0 bg-[url('/bg1.jpg')] opacity-25 bg-cover bg-fixed"></div>
+        </div>
+        <div className="absolute inset-0 flex flex-col justify-center items-center">
+          <div className="max-w-7xl mx-auto p-4 text-center text-white">
+            <h1 className="text-6xl mt-4 font-sans pb-4">CamelidCoin</h1>
+            <h4 className="text-2xl font-bold font-sans">Open-Source. Distributed. Censorship-Free.</h4>
+            <p className="pt-4 w-1/2 mx-auto">Our groundbreaking blockchain protocol is tailored for distributed GPT model computation. Be a part of this paradigm shift, earning rewards for your contributions. Enjoy a network that champions freedom of expression, untouched by censorship. Discover the true potential of decentralized AI!</p>
+            <a href="/learn" className="mt-4 bg-primary/75 hover:bg-primary/100 py-2 px-4 inline-block text-black font-bold rounded"> Learn More </a>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="sticky top-[100vh] bg-primary w-full text-center p-4 text-xs">
-      &#169; 2023 CamelCoin | Released under the GPLv3 License
-    </div>
     </>
-  )
+  );
 }
